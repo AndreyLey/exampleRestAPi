3aa068823825497718e0171f80de450c6e82cb98
Andrey <andreyleykin@gmail.com>
2018-06-04 23:37:19 +0300

    upload project

---
+++ src/main/java/com/example/rapi/exampleRestAPi/ExampleRestAPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rapi.exampleRestAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+		ExampleRestAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleRestAPiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/rapi/exampleRestAPi/ExampleRestAPiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rapi.exampleRestAPi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleRestAPiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
